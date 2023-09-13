40ef048347a18160f2a47673b75ecdf18bc8615e
duc tran <ductm@edmicro.vn>
2023-09-13 17:05:55 +0700

    iterator

---
+++ DesignPatterns/Iterator.cs
@@ -0,0 +1,52 @@
+﻿namespace OOP.DesignPatterns
+{
+    /**
+     * The Iterator design pattern provides a way to access the elements of an aggregate (tổng hợp) object sequentially(tuần tự) without exposing 
+     * its underlying (adj) ['ʌndə,laiiη] cơ bản representation.
+     * 
+     * Frequency (N - Tính thường xuyên) of use: high (Tần suất sử dụng)
+     */
+    /**
+     * The classes and objects participating in this pattern include:
+        1. Iterator  (AbstractIterator)
+        defines an interface for accessing and traversing elements. (duyệt từng phần tử)
+        2. ConcreteIterator  (Iterator)
+        implements the Iterator interface.
+        keeps track of the current position in the traversal of the aggregate. (theo dõi vị trí hiện tại trong quá trình truyền tải)
+        3. Aggregate  (AbstractCollection)
+        defines an interface for creating an Iterator object
+        4. ConcreteAggregate  (Collection)
+        implements the Iterator creation interface to return an instance of the proper ['prɔpə] (adj - đúng, thích hợp) ConcreteIterator
+     */
+    public interface IIterator
+    {
+        Iterator CreateIterator();
+    }
+
+    public class Item
+    {
+        string name;
+        public Item(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name { 
+            get { return name;  }
+        }
+    }
+
+    public class Iterator: IIterator 
+    {
+        List<Item> items = new List<Item>();
+        public Iterator CreateIterator()
+        {
+            return new Iterator();
+        }
+
+        public int Count
+        {
+            get { return items.Count;  }
+        }
+    }
+}
--- DesignPatterns/SimulatorUDuck.cs
+++ DesignPatterns/SimulatorUDuck.cs
@@ -1,4 +1,8 @@
-﻿namespace OOP.DesignPatterns
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace OOP.DesignPatterns
 {
     public class SimulatorUDuck
     {
@@ -18,15 +22,15 @@
         }
     }
 
-    public class RedheadDuck: Quackable
+    public class RedheadDuck : Quackable
     {
         public void quack()
         {
-            Console.WriteLine("Quack");
+            Console.WriteLine("Redhead Quack");
         }
     }
 
-    public class DuckCall: Quackable
+    public class DuckCall : Quackable
     {
         public void quack()
         {
@@ -88,6 +92,59 @@
     // Cần 1 nhà máy sản xuất vịt ( chúng tôi cần kiểm soát chất lượng để đảm bảo rằng vịt của chúng tôi sẽ được bọc)
     // Nhà máy là sản xuất 1 bộ các sản phẩm includes các type duck khác nhau (use Abstract Factory pattern)
     // (Vì vậy, làm thế nào để bạn đảm bảo mỗi nhượng quyền đang sử dụng các thành phần chất lượng hay không? Bạn sẽ xây dựng một factory sản xuất chúng và chuyển chúng đến nhượng quyền của bạn!)
+    public abstract class AbstractDuckFactory {
+        public abstract Quackable createMallardDuck();
+        public abstract Quackable createRedheadDuck();
+        public abstract Quackable createDuckCall();
+    }
+
+    // A Factory to create product
+    public class DuckFactory: AbstractDuckFactory
+    {
+        public override Quackable createMallardDuck()
+        {
+            return new MallardDuck();
+        }
+        public override Quackable createRedheadDuck()
+        {
+            return new RedheadDuck();
+        }
+        public override Quackable createDuckCall()
+        {
+            return new DuckCall();
+        }
+    }
+
+    public class CountingDuckFactory: AbstractDuckFactory {
+        public override Quackable createMallardDuck()
+        {
+            return new QuackCounterDecorator(new MallardDuck());
+        }
+        public override Quackable createRedheadDuck()
+        {
+            return new QuackCounterDecorator(new RedheadDuck());
+        }
+        public override Quackable createDuckCall()
+        {
+            return new QuackCounterDecorator(new DuckCall());
+        }
+    }
+
+    // Composite Pattern
+    // Flock cụm, đám đông
+    public class Flock: Quackable
+    {
+        ArrayList quackers = new ArrayList();
+
+        public void add(Quackable quacker)
+        {
+            quackers.Add(quacker);
+        }
+
+        public void quack()
+        {
+        }
+    }
 
     // 1 trình giả lập Simulator
     public class DuckSimulator
@@ -96,6 +153,8 @@
         {
             DuckSimulator simulator = new DuckSimulator();
             simulator.simulate();
+
+            simulator.simulateFactory(new CountingDuckFactory());
         }
 
         void simulate()
@@ -112,6 +171,19 @@
 
             Quackable mallarDuckDecorator = new QuackCounterDecorator(new MallardDuck());
             simulate(mallarDuckDecorator);
+            Console.WriteLine($"The ducks quacked {QuackCounterDecorator.getQuacks()} times");
+        }
+
+        void simulateFactory(AbstractDuckFactory abstractDuckFactory)
+        {
+            Quackable mallarDuck = abstractDuckFactory.createDuckCall();
+            Quackable redheadDuck = abstractDuckFactory.createMallardDuck();
+            Quackable duckCall = abstractDuckFactory.createRedheadDuck();
+            Quackable gooseDuck = new GooseAdapter(new Goose());
+            simulate(mallarDuck);
+            simulate(redheadDuck);
+            simulate(duckCall);
+            simulate(gooseDuck);
             Console.WriteLine($"The ducks quacked {QuackCounterDecorator.getQuacks()} times");
         }
 
